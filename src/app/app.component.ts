184747615ccc0b604e8314161428035d39d727cf
Pavan Mundada <pavanpmundada5793@gmail.com>
2023-06-05 18:48:07 +0530

    property binding and interpollation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,30 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '16';
-  displayVal ="";
-  displayValforInput ="";
-  count = 0;
-  getName(name: any){
-    return  alert(name)
-  }
 
-  getData(value: any){
-     console.log('Hey I am the event!!', value)
-  }
-
-  getValue(val:string){
-      this.displayVal = val;
-     console.warn('This is input element value displayed on screen', val)
-  }
-
-  getValueforInput(val:string){
-   this.displayValforInput =val
-   console.warn('This is input element value displayed on screen', val)
-}
-
-counter(type:string){
-  type === 'add' ? this.count++ : this.count--
-}
-
+  name = "peter"
+  disabled = false;
 
 }
