41469b08b13ef52b44ed9429a1c2459b70025810
Pavan Mundada <pavanpmundada5793@gmail.com>
2023-06-05 11:32:18 +0530

    added inline-style in user-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { CloverComponent } from './clover/clover.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CloverComponent
+    CloverComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule
