a5fa3b602a63d7dc12ca10322d071ee3299e6dd8
Pavan Mundada <pavanpmundada5793@gmail.com>
2023-06-06 11:56:50 +0530

    for-loop-example for both array elements & array objects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,36 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Hey Angularrrrrrrrrrrrrr!';
-  color = 'black';
+  users = ['Pavan', 'Yash', 'Ankit', 'Ganesh', 'Digi'];
 
+  userDetail = [
+    {
+      name: 'Pavan',
+      email: 'pavan@gmail.com',
+      phone: 9730515919,
+      company: 'cloverinfotech',
+    },
+    {
+      name: 'Yash',
+      email: 'Yash@gmail.com',
+      phone: 99293274923,
+      company: 'Atos',
+    },
+    {
+      name: 'Ankit',
+      email: 'ankit@gmail.com',
+      phone: 79793274923,
+      company: 'Accenture',
+    },
+    {
+      name: 'Digi',
+      email: 'Digi@gmail.com',
+      phone: 987453475834,
+      company: 'Tcs',
+    },
+  ];
 }
