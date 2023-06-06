c2c814dd064e516475f863facaf9a304cb158f0e
Pavan Mundada <pavanpmundada5793@gmail.com>
2023-06-06 12:32:26 +0530

    printed multidimensional array usinf *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,24 +15,28 @@ export class AppComponent {
       email: 'pavan@gmail.com',
       phone: 9730515919,
       company: 'cloverinfotech',
+      socialAccounts: ['Insta', 'facebook', 'LinkedIn'],
     },
     {
       name: 'Yash',
       email: 'Yash@gmail.com',
       phone: 99293274923,
       company: 'Atos',
+      socialAccounts: ['Youtube', 'facebook', 'LinkedIn'],
     },
     {
       name: 'Ankit',
       email: 'ankit@gmail.com',
       phone: 79793274923,
       company: 'Accenture',
+      socialAccounts: ['Insta', 'Gmail', 'Yahoo'],
     },
     {
       name: 'Digi',
       email: 'Digi@gmail.com',
       phone: 987453475834,
       company: 'Tcs',
+      socialAccounts: ['Bingo', 'Apple', 'LinkedIn'],
     },
   ];
 }
