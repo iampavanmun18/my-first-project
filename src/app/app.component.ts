121a9447b567a5df602b24053bae7794ba82cbaf
Pavan Mundada <pavanpmundada5793@gmail.com>
2023-06-06 11:03:55 +0530

    switch case example using *ngSwitchCase [ngSwitch]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '16';
-
-  name = "peter"
-  disabled = false;
-  show= false;
-  isNewUser = true
+  title = 'Hey Angularrrrrrrrrrrrrr!';
+  color = 'black';
 
 }
