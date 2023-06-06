384a690bde99563a8adfc1e9fee4cfbe2c13407c
Pavan Mundada <pavanpmundada5793@gmail.com>
2023-06-06 12:53:52 +0530

    added style-binding using [style.color | style.backgroundColor]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,37 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Hey Angularrrrrrrrrrrrrr!';
-  users = ['Pavan', 'Yash', 'Ankit', 'Ganesh', 'Digi'];
-
-  userDetail = [
-    {
-      name: 'Pavan',
-      email: 'pavan@gmail.com',
-      phone: 9730515919,
-      company: 'cloverinfotech',
-      socialAccounts: ['Insta', 'facebook', 'LinkedIn'],
-    },
-    {
-      name: 'Yash',
-      email: 'Yash@gmail.com',
-      phone: 99293274923,
-      company: 'Atos',
-      socialAccounts: ['Youtube', 'facebook', 'LinkedIn'],
-    },
-    {
-      name: 'Ankit',
-      email: 'ankit@gmail.com',
-      phone: 79793274923,
-      company: 'Accenture',
-      socialAccounts: ['Insta', 'Gmail', 'Yahoo'],
-    },
-    {
-      name: 'Digi',
-      email: 'Digi@gmail.com',
-      phone: 987453475834,
-      company: 'Tcs',
-      socialAccounts: ['Bingo', 'Apple', 'LinkedIn'],
-    },
-  ];
+  color = 'red';
+  bgcolor = 'green';
 }
