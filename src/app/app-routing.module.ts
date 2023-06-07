1ace21e86fba36b0347989320a977854d0114134
Pavan Mundada <pavanpmundada5793@gmail.com>
2023-06-07 17:15:11 +0530

    basic routing-example in angular

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'user', component: UserComponent },
+  { path: '', component: HomeComponent },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,10 @@ import { FormsModule } from '@angular/forms';
 import { UsdINRPipe } from './pipes/usd-inr.pipe';
 import { ReactiveFormsModule } from "@angular/forms";
 import { RedElementDirective } from './red-element.directive';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { AboutComponent } from './about/about.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,9 +25,12 @@ import { RedElementDirective } from './red-element.directive';
     ChildComponent,
     UsdINRPipe,
     RedElementDirective,
+    HomeComponent,
+    UserComponent,
+    AboutComponent,
   ],
   //  FormsModule for two way data binding
-  imports: [BrowserModule, BrowserAnimationsModule, FormsModule, ReactiveFormsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, FormsModule, ReactiveFormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
